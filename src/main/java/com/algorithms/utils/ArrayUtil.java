1f502d57170a4312b87e3734e1412397745a684f
venkat_raji <venkat_raji@venkat_raji-PC>
2016-11-19 23:53:53 +0530

    done spiral

--- src/main/java/com/algorithms/utils/ArrayUtil.java
+++ src/main/java/com/algorithms/utils/ArrayUtil.java
@@ -10,42 +10,46 @@ public class ArrayUtil {
 	private static int x_boundary = 0;
 	private static int y_boundary = 0;
 	private static int count = 0;
-
+	private static int total=0;
 	public static void main(String args[]) {
-		printSpiral(4, 4);
+		printSpiral(1, 1);
 	}
 
 	private static void printSpiral(int x, int y) {
 		x_boundary = x - 1;
 		y_boundary = y - 1;
-		int toatal = x * y;
-		while (count != toatal) {
-			walk(RIGHT, toatal);
-			x_location = x_location + 1;
-			walk(DOWN, toatal);
-			y_location = y_location - 1;
-			walk(LEFT, toatal);
-			x_location = x_location - 1;
-			x_boundary=x_boundary-2;
-			y_boundary=y_boundary-1;
-			walk(UP, toatal);
-			y_location = y_location + 1;
+		total = x * y;
+		int left_boundary = 0;
+		int right_boundary = 0;
+		while (count != total) {
+			walk(RIGHT, y_location, y_boundary);
+			if (count != total) {
+				walk(DOWN, x_location + 1, x_boundary);
+			}
+			if (count != total) {
+				walk(LEFT, y_location - 1, left_boundary);
+				left_boundary = left_boundary + 1;
+				right_boundary = right_boundary + 1;
+			}
+			if (count != total) {
+				walk(UP, x_location - 1, right_boundary);
+				x_boundary = x_boundary - 1;
+				y_boundary = y_boundary - 1;
+				y_location = y_location + 1;
+			}
 		}
 	}
 
-	private static void walk(String _command, int total) {
+	private static void walk(String _command, int starting_location, int ending_location) {
 		boolean incerase = (_command.equals(RIGHT) || _command.equals(DOWN));
 		boolean change_y = (_command.equals(RIGHT) || _command.equals(LEFT));
-		boolean is_up = _command.equals(UP);
-		int starting_location = change_y ? y_location : x_location;
-		int ending_location = change_y ? y_boundary : x_boundary;
 		int dummy = 0;
-		for (int local_pointer = starting_location; isValidTermination(incerase, local_pointer, ending_location,
-				is_up); dummy = incerase ? local_pointer++ : local_pointer--) {
+		for (int local_pointer = starting_location; incerase ? local_pointer <= ending_location
+				: local_pointer >= ending_location; dummy = incerase ? local_pointer++ : local_pointer--) {
 			if (count == total) {
 				break;
 			} else {
-				if (change_y ) {
+				if (change_y) {
 					y_location = local_pointer;
 					count = count + 1;
 					System.out.print("( " + x_location + " ," + y_location + " )");
@@ -58,11 +62,5 @@ public class ArrayUtil {
 
 		}
 
-	}
-
-	private static boolean isValidTermination(boolean incerase, int local_pointer, int ending_location, boolean is_up) {
-		return (incerase ? local_pointer <= ending_location
-				: (is_up ? (local_pointer >= ending_location)
-						: (local_pointer >= 0 && local_pointer <= ending_location)));
 	}
 }
